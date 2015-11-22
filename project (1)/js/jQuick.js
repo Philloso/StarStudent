a8a1283d82ace1fddda19d68b2d5b94c692e04f9
Michael Ashabraner <ender8383@live.com>
2015-11-22 17:25:40 -0500

    Star Student

---
+++ project (1)/js/jQuick.js
@@ -0,0 +1,9 @@
+ $(".ball").hide().show("slow");
+ 
+ $(".navbar.navbar-default.navbar-fixed-top").hide().fadeIn("slow");
+
+ $(".device").hide().fadeToggle("slow");
+
+ $(".panel").hide().show("slow");
+
+ $(".col-sm-6").hide().show("slow");
